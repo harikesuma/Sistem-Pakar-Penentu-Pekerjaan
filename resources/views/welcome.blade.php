901480ff3b2685dca38433bdb604a810b934e82a
Nara Surya <ibagungnsd13@gmail.com>
2019-03-26 16:14:50 +0800

    change welcome blade (nar)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sistem Pakar  Penentu Pekerjaan
                 </div>
 
                 <div class="links">
