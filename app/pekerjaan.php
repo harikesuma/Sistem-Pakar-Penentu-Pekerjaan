f83cbb207ff9628ab0e916baf398625670e92623
harikesuma <harikesuma98@gmail.com>
2019-04-07 22:36:13 +0800

    change on pekerjaan.index

--- app/pekerjaan.php
+++ app/pekerjaan.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class pekerjaan extends Model
 {
-    protected $fillable = ['pekerjaan','image','topik_id','label','deskripsi'];
+    protected $fillable = ['pekerjaan','image','topic_id','label','deskripsi'];
 
     public function karakteristik()
     {
         return $this->belongsToMany('App\karakteristik','pekerjaans_karakteristiks','pekerjaan_id','karakteristik_id');
+    }
+
+    public function topic()
+    {
+        return $this->belongsTo('App\Topic');
     }
 }
